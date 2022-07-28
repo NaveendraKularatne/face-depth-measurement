70b46b8f0ab2ea5eae16962b01061ad344544f2e
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-07-28 18:08:40 +0530

    Show depth

--- FaceDepthMeasurement.py
+++ FaceDepthMeasurement.py
@@ -17,6 +17,22 @@ while True:
         cv2.line(img, pointLeft, pointRight, (0, 200, 0), 3)
         cv2.circle(img, pointLeft, 5, (255, 0, 255), cv2.FILLED)
         cv2.circle(img, pointRight, 5, (255, 0, 255), cv2.FILLED)
+        w, _ = detector.findDistance(pointLeft, pointRight)
+        W = 6.3
+
+        # Finding the focal length
+        # d = 68
+        # f = (w*d)/W
+        # print(f)
+
+        # Finding the depth
+        f = 640
+        d = (W*f)/w
+        print(d)
+
+        cvzone.putTextRect(img, f'Depth: {int(d)}cm',
+                           (face[10][0]-75, face[10][1]-50),
+                           scale = 2)
 
     cv2.imshow("Image" ,img )
     cv2.waitKey(1)
